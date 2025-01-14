b31e3a1f7069a0a2cdc11cc442e1bc87e1ed1bf1
Mohmn <naqashmohmin1@gmail.com>
2025-01-14 19:16:00 +0530

    pass multiple delim test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,32 @@
 
+class DelimeterParser:
+    def __init__(self, numbers: str):
+        self.numbers = numbers
+        self.delimiters = set(["\n", ","])
 
+    def parse(self):
+        number = ""
+        num_len = len(self.numbers)
+        delim = ""
+        i = 0
+        while i < num_len:
+            char = self.numbers[i]
+            if char.isdigit():
+                if delim:
+                    delim = ""
+                number += char
+            else:
+                if number:
+                    yield number
+                    number = ""
+                delim += char
+            i += 1
+
+        if number:
+            yield number
 
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
-    return sum(int(item) for item in numbers.split(','))
+    delimeterParser = DelimeterParser(numbers)
+    return sum(int(item) for item in delimeterParser.parse())
